969cc9272a34773267fa38b7426537ed846bc223
23NPA1 . <astin222@list.ru>
2025-10-01 15:27:02 +0300

    taski

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
-
-from api import models
 from django.test import Client, TestCase
+from api import models
 
 
 class TaskiAPITestCase(TestCase):
@@ -17,5 +16,3 @@ class TaskiAPITestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
-
-
